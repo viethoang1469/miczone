56e9f89ff2aec92ce4955cb547a10716ec2aeb69
viethoang1469 <viethoang1541@gmail.com>
2021-03-10 16:22:44 +0700

    first commit

---
+++ src/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=cd, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Miczone</title>
+</head>
+
+<body>
+    <div class="banner">
+        <img src="img/banner.jpg" alt="">
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="#">Trang chu</a></li>
+            <li><a href="#">Danh muc</a></li>
+            <li><a href="#">Tin tuc</a></li>
+            <li><a href="#">Gioi thieu</a></li>
+        </ul>
+    </div>
+    <div class="container">
+        <div>
+            <div class="">
+                <div class="title">
+                    <h2>To do list</h2>
+                    <hr>    
+                </div>
+                <ul id="" class="list-left">
+                    <li><a href="#">All</a></li>
+                    <li><a href="#">Done</a></li>
+                    <li><a href="#">Undone</a></li>
+                </ul>
+                
+            </div>
+            <div class="article">
+                <div class="title">
+                    <h2>News</h2>
+                    <hr>   
+                    
+                </div>
+                <ul id="" class="list-left">
+                    <li><a href="#">New article 1</a></li>
+                    <li><a href="#">New article 2</a></li>
+                    <li><a href="#">New article 3</a></li>
+                    <li><a href="#">New article 4</a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <div id="myDIV" class="header">
+                <h2>My To Do List</h2>
+                <form action="" id="form">
+                    <input type="text" id="myInput" name="name" placeholder="Content...">
+                    <button type="submit"  class="addBtn">Submit</button type="submit">
+                </form>
+            </div>
+            <ul id="list">
+            </ul>
+        </div>
+    </div>
+    <footer>
+        <p>Author: Viet Hoang<br>
+        <a href="mailto:viethoang1541@gmail.com">viethoang1541@gmail.com</a></p>
+      </footer>
+      <script src="js/jquery.js"></script>
+      <script src="js/my-js.js"></script>
+</body>
+
+</html>
---
+++ src/js/my-js.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    let btnDelete = "<span class='deleteBtn'>delete</span>";
+    let btnDone = "<span class='statusBtn done'>done</span>";
+    let btnUndone = "<span class='statusBtn undone'>undone</span>";
+    $.ajax({
+        url: 'http://localhost:8084/api/list.php?action=toDoList',
+        method: 'get',
+        dataType: 'json',
+        success: function (result) {
+            if (result.success === true && result.code === 200) {
+                $.each(result.data, function (i) {
+                    let btn = (result.data[i].status === 'done') ? btnUndone : btnDone ;
+                    let element = '<li data-id = '+ result.data[i].id + '>' + result.data[i].name + btnDelete + btn + '</li>';
+                    $('#list').prepend(element);
+                });
+            }
+        }
+    });
+    $('.addBtn').click(function (e) {
+        e.preventDefault();
+        let frm = $("#form");
+        $.ajax({
+            url: 'http://localhost:8084/api/list.php?action=add',
+            method: 'post',
+            dataType: 'json',
+            data: frm.serialize(),
+            success: function(result) {
+                let element = '<li data-id = '+ result.data.id + '>' + result.data.name + btnDelete + btnDone + '</li>';
+                $('#list').prepend(element);
+                $('#myInput').val('');
+                alert(result.message);
+            }
+        });
+
+    });
+    $('#list').on("click", '.statusBtn', function () {
+        let flag = false;
+        let id = $(this).parent().data('id');
+        let old_status = ($(this).text() === 'undone') ? 'done' : 'not_done_yet';
+        let element = $(this);
+        $.get('http://localhost:8084/api/list.php?action=edit&id=' + id + '&old_status=' + old_status, function( result ) {
+            if(result.success === true){
+                flag = true;
+                changeStatus(element);
+                alert(result.message);
+            }
+        });
+        
+        // changeStatus(element);
+    });
+    $('#list').on("click", '.deleteBtn', function () {
+        let id = $(this).parent().data('id');
+        let flag = false;
+        $.get('http://localhost:8084/api/list.php?action=delete&id=' + id, function( result ) {
+            if(result.success === true){
+                $('[data-id="' + id + '"]').css('display', 'none');
+                flag = true;
+                alert(result.message);
+            }
+        });
+    });
+});
+function changeStatus(element) {
+    let oldValue = $(element).text();
+    let newValue = (oldValue == 'done') ? 'undone' : 'done';
+    $(element).text(newValue);
+    $(element).removeClass(oldValue);
+    $(element).addClass(newValue);
+}
+
